3b143aa3bbdfb2eb57ea944901bce2647c8cdd4f
Gian Maria <ricci.gianmaria@gmail.com>
2021-12-03 06:47:37 +0000

    I'm a 'pippa' in powershell :D

---
+++ Alkampfer/03/01.ps1
@@ -0,0 +1,27 @@
+$dataLines = Get-Content './input.txt'
+$lines = $dataLines.Length
+$data = $dataLines | Join-String
+$counter = [int[]]::new(12);
+
+for ($i = 0; $i -lt $data.Length; $i++) {
+    $counter[$i % 12] += $data[$i] - 48
+}
+ 
+Write-Host $counter
+$gammaBinary = ""
+$epsilonBinary = ""
+foreach ($item in $counter) {
+    if ($item -gt  $dataLines.Length / 2) { 
+        $gammaBinary+='1'
+        $epsilonBinary+='0'
+    } else {
+        $gammaBinary+='0'
+        $epsilonBinary+='1'
+    }
+}
+Write-Host $gammaBinary
+Write-Host $epsilonBinary
+$gamma = [Convert]::ToInt32($gammaBinary, 2)
+$epsilon = [Convert]::ToInt32($epsilonBinary, 2)
+$result = $gamma * $epsilon
+Write-Host $result
---
+++ Alkampfer/03/02.ps1
@@ -0,0 +1,43 @@
+function Get-CountOfOnes([System.Collections.ArrayList]$array, [int]$position) {
+    $countOfOnes = 0
+    $array | % {$countOfOnes += $_[$i] - 48}
+    return $countOfOnes
+}
+
+function Remove-NonCompatibleElement([System.Collections.ArrayList]$arrayList, [int]$position, [char]$expected) {
+    if ($arrayList.Count -gt 1) {
+        foreach ($item in $arrayList.Clone()) {
+            if ($item[$position] -ne $expected) {
+                $arrayList.remove($item)
+            }
+        }
+    }
+}
+
+$data = Get-Content './input.txt'
+
+$oxy = [System.Collections.ArrayList]$data
+$co2 = [System.Collections.ArrayList]$data
+# Ok cicle for each bit starting from leftmost to rightmost
+for ($i = 0; $i -lt 12; $i++) {
+    $oxyCountOfOnes = Get-CountOfOnes -array $oxy -position $i
+    $co2CountOfOnes = Get-CountOfOnes -array $co2 -position $i
+    if ($oxyCountOfOnes -ge $oxy.Count / 2) {
+        Remove-NonCompatibleElement -arraylist $oxy -position $i -expected '1' 
+    } else {
+        Remove-NonCompatibleElement -arraylist $oxy -position $i -expected '0' 
+    }
+    if ($co2CountOfOnes -ge $co2.Count / 2) {
+        Remove-NonCompatibleElement -arraylist $co2 -position $i -expected '0' 
+    } else {
+        Remove-NonCompatibleElement -arraylist $co2 -position $i -expected '1' 
+    }
+}
+
+Write-Host $oxy
+Write-Host $co2
+
+$decimalOxy = [Convert]::ToInt32($oxy, 2)
+$decimalCo2 = [Convert]::ToInt32($co2, 2)
+$result = $decimalOxy * $decimalCo2
+Write-Host $result
