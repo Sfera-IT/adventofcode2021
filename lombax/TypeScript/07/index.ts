2478ea051e763ba713299ebff895dc697ead62f0
Fabio Lombardo <lombax85@gmail.com>
2021-12-07 12:46:30 +0100

    [LombaX] - day 7

---
+++ lombax/TypeScript/07/index.ts
@@ -0,0 +1,56 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const currentPath : string = path.join(__dirname);
+const input : string = fs.readFileSync(currentPath+'/input.txt','utf8');
+let crabs : Array<number> = input.split(",").map(v => parseInt(v));
+
+
+// TODO optimize to pre-calculate all summations?
+function summation(to) {
+    let sum = 0;
+    for (let i = 0; i <= to; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+
+crabs.sort((a,b) => a - b);
+
+const min = crabs[0];
+const max = crabs[crabs.length-1];
+
+let solutions = [];
+for (let i = min; i <= max; i++) {
+    let fuel = crabs.map(v => Math.abs(v-i)).reduce((a,b) => a+b);
+    solutions[i] = fuel;
+}
+
+solutions.sort((a,b) => a - b);
+
+console.log('Solution 1: '+solutions[0]);
+
+
+solutions = [];
+for (let i = min; i <= max; i++) {
+    let fuel = crabs.map(v => summation(Math.abs(v-i))).reduce((a,b) => a+b);
+    solutions[i] = fuel;
+}
+
+solutions.sort((a,b) => a - b);
+
+console.log('Solution 2: '+solutions[0]);
+
+crabs.sort((a,b) => a - b);
+
+let median = crabs[Math.round(crabs.length/2)];
+let fuel = crabs.map(v => Math.abs(v-median)).reduce((a,b) => a+b);
+console.log('Optimal Solution 1 - use the median value (is it correct or I am only lucky?): ' + fuel);
+
+// let t = crabs.map(v => summation(Math.abs(v-median)));
+// fuel = crabs.map(v => summation(Math.abs(v-median))).reduce((a,b) => a+b);
+// console.log('Optimal Solution 2: ' + fuel);
+
+
+console.log('end');
