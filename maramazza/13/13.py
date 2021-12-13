6694927a52a47fb2993db223a35bc2b4044dc3c2
Matteo <mazzoli.matteo@gmail.com>
2021-12-13 08:01:54 +0100

    [Maramazza] Day 13

---
+++ maramazza/13/13.py
@@ -0,0 +1,26 @@
+with open("13.txt") as f:
+    points, folds = f.read().strip().split("\n\n")
+
+points = {tuple(map(int, line.split(","))) for line in points.split("\n")}
+for i, line in enumerate(folds.split("\n")):
+    coordinate, n = line.split()[-1].split("=")
+    n = int(n)
+    for x, y in list(points):
+        points.remove((x, y))
+        if coordinate == "x" and x > n:
+            x = 2 * n - x
+
+        if coordinate == "y" and y > n:
+            y = 2 * n - y
+
+        points.add((x, y))
+
+    if i == 0:
+        print(len(points))
+
+X, Y = zip(*points)
+for y in range(max(Y) + 1):
+    for x in range(max(X) + 1):
+        print(" #"[(x, y) in points], end="")
+
+    print()
